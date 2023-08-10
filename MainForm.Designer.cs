70740ee05b8d46c22f47a0ecf46981c72226157f
ibrahim.benli <ibrahim.benli@geos.com.tr>
2023-08-10 16:18:25 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RS232CommunicationExample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serialPortComboBox = new System.Windows.Forms.ComboBox();
+            this.richTextBoxReceivedData = new System.Windows.Forms.RichTextBox();
+            this.textBoxSendData = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.txtBaudRate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialPortComboBox
+            // 
+            this.serialPortComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.serialPortComboBox.FormattingEnabled = true;
+            this.serialPortComboBox.Location = new System.Drawing.Point(57, 25);
+            this.serialPortComboBox.Name = "serialPortComboBox";
+            this.serialPortComboBox.Size = new System.Drawing.Size(339, 32);
+            this.serialPortComboBox.TabIndex = 0;
+            // 
+            // richTextBoxReceivedData
+            // 
+            this.richTextBoxReceivedData.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBoxReceivedData.Location = new System.Drawing.Point(57, 66);
+            this.richTextBoxReceivedData.Name = "richTextBoxReceivedData";
+            this.richTextBoxReceivedData.Size = new System.Drawing.Size(339, 288);
+            this.richTextBoxReceivedData.TabIndex = 1;
+            this.richTextBoxReceivedData.Text = "";
+            // 
+            // textBoxSendData
+            // 
+            this.textBoxSendData.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSendData.Location = new System.Drawing.Point(57, 373);
+            this.textBoxSendData.Name = "textBoxSendData";
+            this.textBoxSendData.Size = new System.Drawing.Size(339, 29);
+            this.textBoxSendData.TabIndex = 2;
+            this.textBoxSendData.Text = "Sending Write Message:";
+            this.textBoxSendData.Click += new System.EventHandler(this.textBoxSendData_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSend.Location = new System.Drawing.Point(57, 465);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(339, 55);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonClose.Location = new System.Drawing.Point(266, 526);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(130, 55);
+            this.buttonClose.TabIndex = 4;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonOpen.Location = new System.Drawing.Point(57, 526);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(130, 55);
+            this.buttonOpen.TabIndex = 5;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // txtBaudRate
+            // 
+            this.txtBaudRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBaudRate.Location = new System.Drawing.Point(57, 408);
+            this.txtBaudRate.Multiline = true;
+            this.txtBaudRate.Name = "txtBaudRate";
+            this.txtBaudRate.Size = new System.Drawing.Size(339, 29);
+            this.txtBaudRate.TabIndex = 6;
+            this.txtBaudRate.Text = "DESİRED BAUD RATE :";
+            this.txtBaudRate.Click += new System.EventHandler(this.txtBaudRate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 609);
+            this.Controls.Add(this.txtBaudRate);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSendData);
+            this.Controls.Add(this.richTextBoxReceivedData);
+            this.Controls.Add(this.serialPortComboBox);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox serialPortComboBox;
+        private System.Windows.Forms.RichTextBox richTextBoxReceivedData;
+        private System.Windows.Forms.TextBox textBoxSendData;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.TextBox txtBaudRate;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace RS232CommunicationExample
+{
+    public partial class MainForm : Form
+    {
+        private SerialPort serialPort;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Populate the ComboBox with available serial ports
+            string[] availablePorts = SerialPort.GetPortNames();
+            serialPortComboBox.Items.AddRange(availablePorts);
+            serialPortComboBox.Text = availablePorts[0];
+            serialPort = new SerialPort();
+            serialPort.DataReceived += SerialPort_DataReceived;
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string receivedData = serialPort.ReadExisting();
+            Invoke(new Action(() => richTextBoxReceivedData.AppendText(receivedData)));
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.PortName = serialPortComboBox.SelectedItem.ToString();
+                buttonOpen.Enabled = false;
+                buttonClose.Enabled = true;
+                serialPort.BaudRate = int.Parse(txtBaudRate.Text); // Set your desired baud rate
+                serialPort.Open();
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine(textBoxSendData.Text);
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            buttonClose.Enabled = false;
+            buttonOpen.Enabled = true;
+        }
+        private void txtBaudRate_Click(object sender, EventArgs e)
+        {
+            txtBaudRate.Text = string.Empty;
+        }
+
+        private void textBoxSendData_Click(object sender, EventArgs e)
+        {
+            textBoxSendData.Text = string.Empty;
+        }
+    }
+}
